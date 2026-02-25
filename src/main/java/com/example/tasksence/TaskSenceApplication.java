e0480cc67182ee7b05c91e6c61467624f3e64aee
Alro127 <nhuttan373@gmail.com>
2026-02-25 22:18:17 +0700

    first commit

---
+++ src/main/java/com/example/tasksence/TaskSenceApplication.java
@@ -0,0 +1,13 @@
+package com.example.tasksence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskSenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskSenceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tasksence/TaskSenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tasksence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskSenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
