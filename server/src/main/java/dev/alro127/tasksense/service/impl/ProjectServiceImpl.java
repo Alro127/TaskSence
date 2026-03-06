8ceb062e4713f7f098aa159ac3cbf43afa38d8c7
MihnNathja <150262217+MihnNathja@users.noreply.github.com>
2026-03-06 14:07:39 +0700

    fix: change getProjectByWorkspace API to load all project that user joined

--- server/src/main/java/dev/alro127/tasksense/service/impl/ProjectServiceImpl.java
+++ server/src/main/java/dev/alro127/tasksense/service/impl/ProjectServiceImpl.java
@@ -12,9 +12,11 @@ import dev.alro127.tasksense.exception.ResourceNotFoundException;
 import dev.alro127.tasksense.exception.UnauthorizedException;
 import dev.alro127.tasksense.repository.jpa.ProjectMemberRepository;
 import dev.alro127.tasksense.repository.jpa.ProjectRepository;
+import dev.alro127.tasksense.repository.jpa.WorkspaceMemberRepository;
 import dev.alro127.tasksense.repository.jpa.WorkspaceRepository;
 import dev.alro127.tasksense.service.ProjectService;
 import dev.alro127.tasksense.service.SecurityService;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -27,10 +29,12 @@ public class ProjectServiceImpl implements ProjectService {
 
     private final ProjectRepository projectRepository;
     private final WorkspaceRepository workspaceRepository;
+    private final WorkspaceMemberRepository workspaceMemberRepository;
     private final ProjectMemberRepository projectMemberRepository;
     private final SecurityService securityService;
 
     @Override
+    @Transactional
     public ProjectResponse createProject(Long workspaceId, CreateProjectRequest request) {
         UserEntity currentUser = securityService.getCurrentUser();
 
@@ -71,8 +75,8 @@ public class ProjectServiceImpl implements ProjectService {
         ProjectEntity project = projectRepository.findById(projectId)
                 .orElseThrow(() -> new ResourceNotFoundException("Project not found"));
 
+        validateWorkspaceMemberAccess(workspaceId, currentUser.getId());
         validateProjectBelongsToWorkspace(project, workspaceId);
-
         validateProjectAccess(workspace, project, currentUser.getId());
 
         return ProjectResponse.mapToResponse(project);
@@ -82,12 +86,12 @@ public class ProjectServiceImpl implements ProjectService {
     public List<ProjectResponse> getProjectsByWorkspace(Long workspaceId) {
         UserEntity currentUser = securityService.getCurrentUser();
 
-        WorkspaceEntity workspace = workspaceRepository.findById(workspaceId)
+        workspaceRepository.findById(workspaceId)
                 .orElseThrow(() -> new ResourceNotFoundException("Workspace not found"));
 
-        validateWorkspaceOwnership(workspace, currentUser.getId());
+        validateWorkspaceMemberAccess(workspaceId, currentUser.getId());
 
-        return projectRepository.findAllByWorkspaceId(workspaceId)
+        return projectRepository.findAllByWorkspaceIdAndMemberId(workspaceId, currentUser.getId())
                 .stream()
                 .map(ProjectResponse::mapToResponse)
                 .toList();
@@ -104,7 +108,6 @@ public class ProjectServiceImpl implements ProjectService {
                 .orElseThrow(() -> new ResourceNotFoundException("Project not found"));
 
         validateProjectBelongsToWorkspace(project, workspaceId);
-
         validateProjectManagerAccess(workspace, project, currentUser.getId());
 
         if (request.getName() != null) {
@@ -139,7 +142,6 @@ public class ProjectServiceImpl implements ProjectService {
                 .orElseThrow(() -> new ResourceNotFoundException("Project not found"));
 
         validateProjectBelongsToWorkspace(project, workspaceId);
-
         validateProjectManagerAccess(workspace, project, currentUser.getId());
 
         project.setDeletedAt(OffsetDateTime.now());
@@ -158,6 +160,14 @@ public class ProjectServiceImpl implements ProjectService {
     private void validateWorkspaceOwnership(WorkspaceEntity workspace, Long userId) {
         if (!workspace.getOwner().getId().equals(userId)) {
             throw new UnauthorizedException("Access denied");
+        }
+    }
+
+    private void validateWorkspaceMemberAccess(Long workspaceId, Long userId) {
+        boolean isWorkspaceMember = workspaceMemberRepository.existsByWorkspaceIdAndUserId(workspaceId, userId);
+
+        if (!isWorkspaceMember) {
+            throw new UnauthorizedException("You are not a member of this workspace");
         }
     }
 
