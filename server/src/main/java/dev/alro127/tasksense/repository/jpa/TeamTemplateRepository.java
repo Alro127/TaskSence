3a295267ee9650a4dbf58c689b34132db870e1f6
MihnNathja <150262217+MihnNathja@users.noreply.github.com>
2026-03-13 10:47:59 +0700

    feat: implement pagination for team templates retrieval in controller, service, and repository

--- server/src/main/java/dev/alro127/tasksense/repository/jpa/TeamTemplateRepository.java
+++ server/src/main/java/dev/alro127/tasksense/repository/jpa/TeamTemplateRepository.java
@@ -2,15 +2,14 @@ package dev.alro127.tasksense.repository.jpa;
 
 import dev.alro127.tasksense.domain.entity.TeamTemplateEntity;
 import org.jspecify.annotations.NullMarked;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import java.util.List;
 import java.util.Optional;
 
 public interface TeamTemplateRepository extends JpaRepository<TeamTemplateEntity, Long> {
 
-    List<TeamTemplateEntity> findByOwnerId(Long ownerId);
+    Page<TeamTemplateEntity> findByOwnerId(Long ownerId, Pageable pageable);
 
     @NullMarked
     Optional<TeamTemplateEntity> findById(Long id);
